e828c606d9c7fb4ad714e00173dd9178264c5a5f
Lean 4 VS Code Extension <>
2024-06-18 14:47:27 +0000

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «Leanprojectcoolio2»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «leanprojectcoolio2» where
+  -- add package configuration options here
+
+lean_lib «Leanprojectcoolio2» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «leanprojectcoolio2» where
+  root := `Main
